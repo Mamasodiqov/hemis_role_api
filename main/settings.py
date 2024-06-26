57f1b21c0a5f8f1ed04b6816c408d430b1875ac6
feruzLatifov <138076837+feruzLatifov@users.noreply.github.com>
2024-06-26 17:12:37 +0500

    cors-headers

--- main/settings.py
+++ main/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     # pakages
     'rest_framework',
     'drf_yasg',
+    'corsheaders',
 
     # local
     'role',
@@ -56,6 +57,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
